d7caa0b668aa6c92b8701c1a162c6463569bb1b4
JaxonAtBYUI <jaxonhamm@byui.edu>
2022-07-09 14:28:32 -0600

    temporary rotating circles thing

---
+++ scripts/fades.js
@@ -0,0 +1,58 @@
+/* -= ========== FADE RIGHT ========== =- */
+// Get all the images
+const fadeRights = document.querySelectorAll('.fadeRight');
+const options = {threshold: .4, rootMargin:"0px 0px 200px 0px"}
+const fadeLefts = document.querySelectorAll('.fadeLeft');
+
+// Create a new observer
+const ioFR = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+            else {
+                fadeRight(entry.target);
+                io.unobserve(entry.target);
+            }
+        })
+    }, options
+)
+
+// Toggle the classes
+function fadeRight(element) {
+    element.classList.toggle("fadeRight");
+    element.classList.toggle("fadeRightA");
+}
+
+// Check to see if the images are on screen.
+fadeRights.forEach(element => {
+    ioFR.observe(element);
+})
+
+/* -= ========== FADE LEFT ========== =- */
+// Create a new observer
+const ioFL = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+            else {
+                fadeLeft(entry.target);
+                io.unobserve(entry.target);
+            }
+        })
+    }, options
+)
+
+// Toggle the classes
+function fadeLeft(element) {
+    element.classList.toggle("fadeLeft");
+    element.classList.toggle("fadeLeftA");
+}
+
+// Check to see if the images are on screen.
+fadeLefts.forEach(element => {
+    ioFL.observe(element);
+})
--- scripts/indexGlitch.js
+++ scripts/indexGlitch.js
@@ -5,14 +5,31 @@ const MAX_DELAY_AFTER = 1;
 const RUNNING = true;
 const GI1 = document.getElementById("GI1");
 const GI2 = document.getElementById("GI2");
+const GI3 = document.getElementById("GI3");
+const GI4 = document.getElementById("GI4");
+const GI5 = document.getElementById("GI5");
+const GI6 = document.getElementById("GI6");
 
-let preared = false;
 let before1;
 let before2;
+let before3;
+let before4;
+let before5;
+let before6;
+
 let gTimerB1;
 let gTimerB2;
+let gTimerB3;
+let gTimerB4;
+let gTimerB5;
+let gTimerB6;
+
 let gTimerA1;
 let gTimerA2;
+let gTimerA3;
+let gTimerA4;
+let gTimerA5;
+let gTimerA6;
 
 // GET RANDOM FLOAT
 // Gets and returns a random 1 digit decimal number
@@ -52,12 +69,25 @@ function prepare() {
     // Get our timers
     gTimerB1 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
     gTimerB2 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+    gTimerB3 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+    gTimerB4 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+    gTimerB5 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+    gTimerB6 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+
     gTimerA1 = 0;
     gTimerA2 = 0;
+    gTimerA3 = 0;
+    gTimerA4 = 0;
+    gTimerA5 = 0;
+    gTimerA6 = 0;
 
     // Set our state
     before1 = true;
     before2 = true;
+    before3 = true;
+    before4 = true;
+    before5 = true;
+    before6 = true;
 }
 
 function glitchImages() {
@@ -108,6 +138,106 @@ function glitchImages() {
 
         // Countdown to reversion
         gTimerA2 -= 1
+    }
+
+    // Image 3
+    if (before3) {   
+        // Animation plays
+        if (gTimerB3 <= 0) {
+            distance = randomDistance();
+            GI3.style.left = `${distance}vw`;
+            gTimerA3 = toCounter(getRandomFloat(MAX_DELAY_AFTER));
+            before3 = false;
+        }
+
+        // Countdown until animation
+        gTimerB3 -= 1
+    }
+    else {
+    // Remove animation
+        if (gTimerA3 <= 0) {
+            gTimerB3 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+            GI3.style.left = '0px';
+            before3 = true;
+        }
+
+        // Countdown to reversion
+        gTimerA3 -= 1
+    }
+
+    //Image 4
+    if (before4) {   
+        // Animation plays
+        if (gTimerB4 <= 0) {
+            distance = randomDistance();
+            GI4.style.left = `${distance}vw`;
+            gTimerA4 = toCounter(getRandomFloat(MAX_DELAY_AFTER));
+            before4 = false;
+        }
+
+        // Countdown until animation
+        gTimerB4 -= 1
+    }
+    else {
+        // Remove animation
+        if (gTimerA4 <= 0) {
+            gTimerB4 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+            GI4.style.left = '0';
+            before4 = true;
+        }
+
+        // Countdown to reversion
+        gTimerA4 -= 1
+    }
+
+    //Image 5
+    if (before5) {   
+        // Animation plays
+        if (gTimerB5 <= 0) {
+            distance = randomDistance();
+            GI5.style.left = `${distance}vw`;
+            gTimerA5 = toCounter(getRandomFloat(MAX_DELAY_AFTER));
+            before5 = false;
+        }
+
+        // Countdown until animation
+        gTimerB5 -= 1
+    }
+    else {
+        // Remove animation
+        if (gTimerA5 <= 0) {
+            gTimerB5 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+            GI5.style.left = '0';
+            before5 = true;
+        }
+
+        // Countdown to reversion
+        gTimerA5 -= 1
+    }
+
+    // Images 6
+    if (before6) {   
+        // Animation plays
+        if (gTimerB6 <= 0) {
+            distance = randomDistance();
+            GI6.style.left = `${distance}vw`;
+            gTimerA6 = toCounter(getRandomFloat(MAX_DELAY_AFTER));
+            before6 = false;
+        }
+
+        // Countdown until animation
+        gTimerB6 -= 1
+    }
+    else {
+        // Remove animation
+        if (gTimerA6 <= 0) {
+            gTimerB6 = toCounter(getRandomFloat(MAX_DELAY_BEFORE));
+            GI6.style.left = '0';
+            before6 = true;
+        }
+
+        // Countdown to reversion
+        gTimerA6 -= 1
     }
 }
 prepare();

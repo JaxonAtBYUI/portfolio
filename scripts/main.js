da7eefbd7cacb143a9fa2f4ea7b3ddc8a3d9a47b
JaxonAtBYUI <jaxonhamm@byui.edu>
2022-07-20 22:43:30 -0600

    Fixed some things for mobile.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,4 @@
 /* -~ --- TOGGLE NAVIGATION BAR --- ~- */
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-let openNavigation = false;
-hamburgerMenu.addEventListener("click", toggleNavigation)
-
-function toggleNavigation() {
-    if (openNavigation) {
-        closeNav();
-    }
-}
-
 function openNav() {
   document.getElementById("navBar").style.width = "250px";
 }
